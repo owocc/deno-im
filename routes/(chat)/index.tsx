dc3fb60da2ee61cd687b9af9f911ee6990a7abcb
owocc <owocc@qq.com>
2025-03-27 18:19:06 +0800

    theme

--- routes/(chat)/index.tsx
+++ routes/(chat)/index.tsx
@@ -6,29 +6,39 @@ export default function Home() {
   const name = useSignal<LayoutNames>("default");
   return (
     <LayoutRender name={name}>
-      <ul className="border-r w-44 h-full bg-zinc-50 p-2 flex flex-col">
-        <li className="rounded-xl border border-transparent p-2">
-          <b className="text-xs font-semibold">About Typescript...</b>
-          <p className="text-xs">xxxx</p>
-        </li>
-        <li className="rounded-xl border border-transparent p-2">
-          <b className="text-xs font-semibold">About Typescript...</b>
-          <p className="text-xs">xxxx</p>
-        </li>
-        <li className="rounded-xl border border-transparent p-2">
-          <b className="text-xs font-semibold">About Typescript...</b>
-          <p className="text-xs">xxxx</p>
-        </li>
-        <li className="rounded-xl border border-transparent p-2">
-          <b className="text-xs font-semibold">About Typescript...</b>
-          <p className="text-xs">xxxx</p>
-        </li>
-        <li className="rounded-xl border border-transparent p-2">
-          <b className="text-xs font-semibold">About Typescript...</b>
-          <p className="text-xs">xxxx</p>
-        </li>
-        <li className="bg-white border border-zinc-200 rounded-xl p-2 shadow-sm">
-          <b className="text-xs font-semibold">About Typescript...</b>
+      <ul className="border-r w-64 select-none h-full bg-zinc-50 p-2 flex flex-col overflow-y-auto">
+        {Array.from({ length: 40 }).fill("xxx").map((_e, i) => (
+          <li
+            className="rounded-xl border hover:bg-zinc-100 border-transparent p-1.5"
+            key={i}
+          >
+            <b className="text-xs font-semibold">About Typescript...</b>
+            <p className="text-xs">xxxx</p>
+          </li>
+        ))}
+        <li className="bg-white border border-zinc-200 rounded-xl p-1.5 shadow-sm">
+          <div className="flex items-center justify-between gap-2 w-full">
+            <b className="text-xs font-semibold">
+              About Typescript...
+            </b>
+
+            <button className="p-1 border border-transparent hover:border-zinc-200 hover:shadow-sm rounded-lg">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="size-4"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 12a1 1 0 1 0 2 0a1 1 0 1 0-2 0m7 0a1 1 0 1 0 2 0a1 1 0 1 0-2 0m7 0a1 1 0 1 0 2 0a1 1 0 1 0-2 0"
+                />
+              </svg>
+            </button>
+          </div>
           <p className="text-xs">xxxx</p>
         </li>
       </ul>
